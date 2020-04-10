f217c985b77f307bfeb9b8f5f2301936d4c44f63
Khoa Do <khoado.dk@gmail.com>
2020-04-10 19:11:18 -0400

    Finished the homepage

---
+++ src/components/HomePageComponents/Menu.js
@@ -0,0 +1,65 @@
+import React from "react"
+import { StaticQuery, graphql, Link } from "gatsby"
+import styled from "styled-components"
+
+import Product from "./Product"
+import { Section, Title, SectionButton } from "../../utils"
+
+const PRODUCTS = graphql`
+  {
+    items: allContentfulMenu {
+      edges {
+        node {
+          name
+          id
+          price
+          ingredients
+          img {
+            fixed(height: 150, width: 150) {
+              ...GatsbyContentfulFixed_tracedSVG
+            }
+          }
+        }
+      }
+    }
+  }
+`
+
+const Menu = () => {
+  return (
+    <Section>
+      <Title title="feature items" message="little taste" />
+      <ProductList>
+        <StaticQuery
+          query={PRODUCTS}
+          render={data => {
+            const products = data.items.edges
+            return products.map(product => (
+              <Product key={product.node.id} product={product.node} />
+            ))
+          }}
+        />
+      </ProductList>
+    </Section>
+  )
+}
+
+const ProductList = styled.div`
+  margin: 3rem 0;
+  display: grid;
+  grid-template-columns: 100%;
+  grid-row-gap: 3rem;
+  @media (min-width: 576px) {
+    grid-template-columns: 95%;
+  }
+  @media (min-width: 772px) {
+    grid-template-columns: 80%;
+    justify-content: center;
+  }
+  @media (min-width: 992px) {
+    grid-template-columns: 1fr 1fr;
+    grid-gap: 2rem;
+  }
+`
+
+export default Menu
---
+++ src/components/HomePageComponents/Product.js
@@ -0,0 +1,52 @@
+import React from "react"
+import { StaticQuery, graphql, Link } from "gatsby"
+import styled from "styled-components"
+import Img from "gatsby-image"
+
+import { styles } from "../../utils"
+
+const Product = ({ product }) => {
+  const { name, price, ingredients } = product
+  const { fixed } = product.img
+  return (
+    <ProductWrapper>
+      <Img fixed={fixed} className="img" />
+      <div className="product">
+        <div className="product-content">
+          <h4 className="product-content-name">{name}</h4>
+          <h3 className="product-content-price">${price}</h3>
+        </div>
+        <div className="product-info">{ingredients}</div>
+      </div>
+    </ProductWrapper>
+  )
+}
+
+const ProductWrapper = styled.div`
+  @media (min-width: 576px) {
+    display: grid;
+    grid-template-columns: auto 1fr;
+    grid-column-gap: 1rem;
+  }
+  .img {
+    border-radius: 0.5rem;
+  }
+  .product {
+    background: rgba(210, 170, 92, 0.6);
+    border-radius: 0.5rem;
+    &-content {
+      display: flex;
+      justify-content: space-between;
+      margin: 1rem;
+      font-size: 1.4rem;
+      text-transform: uppercase;
+    }
+    &-info {
+      margin: 1rem;
+      word-spacing: 0.2rem;
+      text-transform: lowercase;
+    }
+  }
+`
+
+export default Product
